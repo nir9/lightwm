3a746c0f124e3dd4c453323e40a444ae8079525e
Nir Lichtman <nir@lichtman.org>
2024-01-22 23:37:12 +0200

    Improve by skipping some windows

--- wm_dll.c
+++ wm_dll.c
@@ -1,9 +1,26 @@
 #include <Windows.h>
 
+HWND managed[256];
+int currentManagedIndex = 0;
+
+BOOL CALLBACK EnumChildProc(HWND hwnd, LPARAM lparam) {
+    if (!IsWindowVisible(hwnd) || !IsHungAppWindow(hwnd)) {
+        return TRUE;
+    }
+
+    managed[currentManagedIndex] = hwnd;
+    currentManagedIndex++;
+    return TRUE;
+}
+
 __declspec(dllexport) LRESULT CALLBACK ShellProc(int code, WPARAM wparam, LPARAM lparam) {
 
     if (code == HSHELL_WINDOWCREATED || code == HSHELL_WINDOWDESTROYED) {
-        TileWindows(NULL, MDITILE_VERTICAL, NULL, NULL, NULL);
+        currentManagedIndex = 0;
+
+        EnumChildWindows(GetDesktopWindow(), EnumChildProc, 0);
+
+        TileWindows(GetDesktopWindow(), MDITILE_VERTICAL | MDITILE_SKIPDISABLED, NULL, currentManagedIndex, managed);
     }
 
     return CallNextHookEx(NULL, code, wparam, lparam);
