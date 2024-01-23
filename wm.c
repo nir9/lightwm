1fc98adeed28daaa390160b1f405dd7e0c27e2ae
DeLuks <dragojeviclk16@gmail.com>
2024-01-23 17:33:47 +0100

    add error handling

--- wm.c
+++ wm.c
@@ -1,19 +1,36 @@
 #include <Windows.h>
 #include <signal.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 HHOOK hookHandle;
 
 void ctrlc(int sig) {
-    UnhookWindowsHookEx(hookHandle);
+    if (!UnhookWindowsHookEx(hookHandle)) {
+        wprintf(L"[!] Error while unhooking hookHandle.\nError: %d\n", GetLastError());
+    }
+    
     exit(0);
 }
 
 int main() {
     HMODULE wmDll = LoadLibraryW(L"wm_dll");
+
+    if (wmDll == NULL) {
+        wprintf( L"[!] Error while loading library.\n Error: %ld", GetLastError());
+    }
+
     FARPROC shellProc = GetProcAddress(wmDll, "ShellProc");
 
+    if (shellProc == NULL) {
+        wprintf(L"[!] Error while getting process address.\nError: %ld", GetLastError());
+    }
+
     hookHandle = SetWindowsHookExW(WH_SHELL, shellProc, wmDll, 0);
+
+    if (hookHandle = NULL) {
+        wprintf(L"[!] Error while getting hook.\nError: %ld", GetLastError());
+    }
 
     signal(SIGINT, ctrlc);
 
