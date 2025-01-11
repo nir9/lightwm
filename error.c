8113b5afb6439c7e1d626eca0efee5f81825aa3e
Nir Lichtman <nir@lichtman.org>
2025-01-11 22:47:27 +0200

    Error: Add win32 last error info to error handling

--- error.c
+++ error.c
@@ -8,5 +8,25 @@ void reportGeneralError(WCHAR* message)
 
 void reportWin32Error(WCHAR* message)
 {
+	DWORD lastError = GetLastError();
+	WCHAR* messageBuffer;
+
 	reportGeneralError(message);
+	if (FormatMessageW(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER |
+		FORMAT_MESSAGE_FROM_SYSTEM |
+		FORMAT_MESSAGE_IGNORE_INSERTS,
+		NULL,
+		lastError,
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(LPWSTR)&messageBuffer,
+		0,
+		NULL) == 0) {
+		reportGeneralError(L"FormatMessageW has failed");
+		return;
+	}
+
+	MessageBoxW(NULL, messageBuffer, L"LightWM Last Win32 Error", MB_OK);
+
+	LocalFree(messageBuffer);
 }
